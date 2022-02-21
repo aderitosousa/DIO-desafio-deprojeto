6ad2dc65abb584e8c3cfa2fa5cf997fde05d00f5
Aderito Sousa <aderitodourado155@gmail.com>
2022-02-21 10:57:12 +0100

    inclusão do prjeto da dio da aula estrutura de dados em java:filas

---
+++ Listaencadeada/src/Listaencadeada/dio/Main.java
@@ -0,0 +1,8 @@
+package Listaencadeada.dio;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ Listaencadeada/src/Listaencadeada/dio/No.java
@@ -0,0 +1,58 @@
+package Listaencadeada.dio;
+
+public class No <T> {
+    private T contudo;
+    private No proximono;
+
+    public No() {
+        this.proximono=null;
+    }
+
+    public No(T contudo) {
+        this.proximono=null;
+        this.contudo = contudo;
+    }
+
+    public No(T contudo, No proximono) {
+        this.contudo = contudo;
+        this.proximono = proximono;
+    }
+
+
+    public T getContudo() {
+        return contudo;
+    }
+
+    public void setContudo(T contudo) {
+        this.contudo = contudo;
+    }
+
+    public No getProximono() {
+        return proximono;
+    }
+
+    public void setProximono(No proximono) {
+        this.proximono = proximono;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "contudo=" + contudo +
+                '}';
+    }
+
+    public String toStringencadeado(){
+        String str= "No{" +
+                "contudo=" + contudo +
+                '}';
+         if( proximono != null){
+             str += "->" + proximono.toString();
+         } else{
+             str +="->null";
+         }
+         return str;
+    }
+
+
+}
